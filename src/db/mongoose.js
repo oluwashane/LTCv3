b1ff7e1d86731d26e01061ed73aedc9466c2567e
oluwashane <ogbulaforudochukwu@gmail.com>
2020-02-25 15:08:40 +0100

    add config file for api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/LTC3', {   
+mongoose.connect(process.env.MONGODB_URL, {   
     useNewUrlParser:true,
     useFindAndModify: false,
     useCreateIndex: true,
